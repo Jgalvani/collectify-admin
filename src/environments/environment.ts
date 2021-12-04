8020bca97bfbd0ac96b84d733a30f50ff94aa77c
jgalvani <jchristophegalvani@gmail.com>
2021-12-04 18:56:08 +0100

    fix: add ts-node & dotenv to package.json

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  API_URL: 'https://b5fe-2a01-cb20-4806-7d00-5cb9-de8b-7a3d-4670.ngrok.io/',
-  TOKEN: '821020a53c4ef5636151fc7ff35c640e4c9881e2',
+  production: false
 };
 
 /*
